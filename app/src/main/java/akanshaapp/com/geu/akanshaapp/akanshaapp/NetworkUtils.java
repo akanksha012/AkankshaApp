a8eace4f1f1ca11f1f304a1be69c16b86bb4e35a
akansha <akansha>
2015-11-14 23:30:53 +0530

    my commitment

---
+++ app/src/main/java/akanshaapp/com/geu/akanshaapp/akanshaapp/NetworkUtils.java
@@ -0,0 +1,35 @@
+package akanshaapp.com.geu.akanshaapp.akanshaapp;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by GB on 11/14/2015.
+ */
+public class NetworkUtils {
+
+
+        private Context _context;
+
+        public NetworkUtils(Context context){
+            this._context = context;
+        }
+
+    public boolean isConnectingToInternet(){
+        ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null)
+        {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                    {
+                        return true;
+                    }
+
+        }
+        return false;
+    }
+}
+
